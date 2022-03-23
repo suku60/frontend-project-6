b124f1e43f9776f336e3fb24e285a8e2df39c3a1
suku60 <juanmaxp2@gmail.com>
2022-03-23 22:10:20 +0100

    Working on animation. main.css seems not to export anything else but what is inside of :root.

--- src/Containers/Home/Home.jsx
+++ src/Containers/Home/Home.jsx
@@ -3,20 +3,41 @@ import './Home.css';
 
 const Home = () => {
 
+    // HOOKS
+
+    // animations
+    const [animComponent, setAnimComponent] = useState("");
+ 
+    // displays
     const [displayLoginButton, setDisplayLoginButton] = useState("none");
     const [displayRegisterButton, setDisplayRegisterButton] = useState("flex");
 
+    // FUNCTIONS
+
+    
+    console.log(animComponent)
+
     const swapButton = () => {
-        
+
         if(displayLoginButton === "flex"){
             setDisplayLoginButton("none")
             setDisplayRegisterButton("flex")
         }else{
+            
+           setAnimComponent("animationContainerFromTop")
             setDisplayLoginButton("flex")
             setDisplayRegisterButton("none")
         }
 
+        
+      setTimeout(() => {
+        setAnimComponent("")
+      }, 1000);
+
+        console.log(animComponent)
     }
+
+    
 
 
 
@@ -24,8 +45,8 @@ return (
     <div className="container_box" id="home_box">
         <div className="login_register_button" id="button_swap" onClick={()=>swapButton()}></div>
 
-        <div className="component_login" style={{display : displayLoginButton}}>
-            <div className='component_box'>
+        <div className="component_login" style={{display : displayLoginButton}} id="animationContainerFromTop">
+            <div className='component_box' id={animComponent}>
             <div className="login_message">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. <br/>
                 Tenetur iure dolorum, <br/>
@@ -38,7 +59,8 @@ return (
             <div className="login_button"></div>
             </div>
         </div>     
-        <div className="component_register" style={{display : displayRegisterButton}}>
+        
+        <div className="component_register" style={{display : displayRegisterButton}} id={animComponent}>
             <div className='component_box' id="register_box">
             <div className="register_inputs"></div>
            
