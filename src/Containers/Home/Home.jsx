546ba583505ea61f49430204fd8305ba7eedbf98
suku60 <juanmaxp2@gmail.com>
2022-03-23 18:42:38 +0100

    Login and register deleted as components; we're going to make login and register as two different boxes inside of home itself.

--- src/Containers/Home/Home.jsx
+++ src/Containers/Home/Home.jsx
@@ -1,17 +1,50 @@
 import React, {useEffect, useState} from 'react';
-import Login from '../../Components/Login/Login';
-import Register from '../../Components/Register/Register';
 import './Home.css';
 
 const Home = () => {
 
+    const [displayLoginButton, setDisplayLoginButton] = useState("none");
+    const [displayRegisterButton, setDisplayRegisterButton] = useState("flex");
+
+    const swapButton = () => {
+        
+        if(displayLoginButton === "flex"){
+            setDisplayLoginButton("none")
+            setDisplayRegisterButton("flex")
+        }else{
+            setDisplayLoginButton("flex")
+            setDisplayRegisterButton("none")
+        }
+
+    }
+
+
+
 return (
     <div className="container_box" id="home_box">
+        <div className="login_register_button" id="button_swap" onClick={()=>swapButton()}></div>
 
-        <div className="login_register_button"></div>
-
-        {/* <Login/> */}
-        <Register/>
+        <div className="component_login" style={{display : displayLoginButton}}>
+            <div className='component_box'>
+            <div className="login_message">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. <br/>
+                Tenetur iure dolorum, <br/>
+                deleniti odit error ad aspernatur. <br/>
+                Consectetur minima, <br/>
+                architecto quod excepturi, <br/>
+                eaque esse quidem ratione odio laboriosam enim ipsum facilis?<br/>
+            </div>
+            <div className="login_inputs"></div>
+            <div className="login_button"></div>
+            </div>
+        </div>     
+        <div className="component_register" style={{display : displayRegisterButton}}>
+            <div className='component_box' id="register_box">
+            <div className="register_inputs"></div>
+           
+            <div className="register_button"></div>
+ 
+       </div></div>
 
     </div>
     )
