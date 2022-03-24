3fef0a48d782cd5e9f5e4ef7397c9c1dbdf3ebc0
suku60 <juanmaxp2@gmail.com>
2022-03-24 01:07:39 +0100

    Progress on animation. Fixed img slider created.

--- src/Containers/Home/Home.jsx
+++ src/Containers/Home/Home.jsx
@@ -7,34 +7,33 @@ const Home = () => {
 
     // animations
     const [animComponent, setAnimComponent] = useState("");
+    const [animButton, setAnimButton] = useState("");
  
     // displays
-    const [displayLoginButton, setDisplayLoginButton] = useState("none");
-    const [displayRegisterButton, setDisplayRegisterButton] = useState("flex");
+    const [displayLoginForm, setDisplayLoginForm] = useState("flex");
+    const [displayRegisterForm, setDisplayRegisterForm] = useState("none");
 
     // FUNCTIONS
 
-    
-    console.log(animComponent)
+    // Function that swaps register/login components
 
     const swapButton = () => {
 
-        if(displayLoginButton === "flex"){
-            setDisplayLoginButton("none")
-            setDisplayRegisterButton("flex")
-        }else{
+
+
+        if(displayLoginForm === "flex"){
             
-           setAnimComponent("animationContainerFromTop")
-            setDisplayLoginButton("flex")
-            setDisplayRegisterButton("none")
+            setAnimComponent("animationContainerFromTop")
+            setAnimButton("animationButtonSwitch")
+            setDisplayLoginForm("none")
+            setDisplayRegisterForm("flex")
+        }else{
+
+            setAnimButton("animationButtonSwitch")
+            setAnimComponent("animationContainerFromTop")
+            setDisplayRegisterForm("none")
+            setDisplayLoginForm("flex")
         }
-
-        
-      setTimeout(() => {
-        setAnimComponent("")
-      }, 1000);
-
-        console.log(animComponent)
     }
 
     
@@ -43,9 +42,30 @@ const Home = () => {
 
 return (
     <div className="container_box" id="home_box">
-        <div className="login_register_button" id="button_swap" onClick={()=>swapButton()}></div>
 
-        <div className="component_login" style={{display : displayLoginButton}} id="animationContainerFromTop">
+{/* THIS WILL BE A CAROUSEL OF IMAGES */}
+
+        <div className="component_login">
+            <div className='component_box'>
+            <div>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. <br/>
+                Tenetur iure dolorum, <br/>
+                deleniti odit error ad aspernatur. <br/>
+                Consectetur minima, <br/>
+                architecto quod excepturi, <br/>
+                eaque esse quidem ratione odio laboriosam enim ipsum facilis?<br/>
+            </div>
+
+            </div>
+        </div>    
+
+{/* HERE WE HAVE THE BUTTON THAT SWITCHES LOGIN/REGISTER FORMS AND THE FORMS */}
+
+
+        <div className="login_register_button" id={animButton} style={{display : displayLoginForm}} onClick={()=>swapButton()}>login</div>
+        <div className="login_register_button" id={animButton} style={{display : displayRegisterForm}} onClick={()=>swapButton()}>register</div>
+
+        <div className="component_login" style={{display : displayLoginForm}}>
             <div className='component_box' id={animComponent}>
             <div className="login_message">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. <br/>
@@ -55,13 +75,28 @@ return (
                 architecto quod excepturi, <br/>
                 eaque esse quidem ratione odio laboriosam enim ipsum facilis?<br/>
             </div>
-            <div className="login_inputs"></div>
+
+            <div className="login_inputs">
+                <input type="name" name="nickname" title="nick" 
+                autoComplete="off"/>
+                <input type="password" name="password" title="pass" 
+                autoComplete="off"/>
+                <input type="email" name="email" title="email" 
+                autoComplete="off"/>
+                <input type="file" name="avatar" title="avatar" 
+                autoComplete="off" id="input_file"/>
+                by clicking here I'm comfirming I am 18 years old or more. 
+                <input type="checkbox" name="avatar" title="avatar" 
+                autoComplete="off"/>
+            
+            </div>
+
             <div className="login_button"></div>
             </div>
         </div>     
         
-        <div className="component_register" style={{display : displayRegisterButton}} id={animComponent}>
-            <div className='component_box' id="register_box">
+        <div className="component_register" style={{display : displayRegisterForm}}>
+            <div className='component_box register_box' id={animComponent}>
             <div className="register_inputs"></div>
            
             <div className="register_button"></div>
