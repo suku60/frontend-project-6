463f768a693f1ec9a7c94634b3684d35c335b8d8
jmonloop <monleonlopezjavier@gmail.com>
2022-03-27 10:18:31 +0200

    working on meme card proportions

--- src/Containers/Search/Search.jsx
+++ src/Containers/Search/Search.jsx
@@ -12,6 +12,13 @@ const Search = () => {
     const [msg, setMsg] = useState("");
     const [searchData, setSearchData] = useState("");
 
+    // stars
+    const [star1, setStar1] = useState("gray");
+    const [star2, setStar2] = useState("gray");
+    const [star3, setStar3] = useState("gray");
+    const [star4, setStar4] = useState("gray");
+    const [star5, setStar5] = useState("gray");
+
     // USEEFFECTS
     useEffect(() => {
         // bringMemes();
@@ -23,7 +30,52 @@ const Search = () => {
     // FUNCTIONS
     // Handlers
     // Local functions
-    // Axios call to map images
+
+    const OnBackgroundStar = () => {
+        // document.getElementById("s1").style.backgroundColor = "red !important";
+        setStar1("red")
+    }
+
+    const OnBackgroundStar2 = () => {
+        // document.getElementById("s1").style.backgroundColor = "red !important";
+        setStar1("red");
+        setStar2("red")
+    }
+
+    const OnBackgroundStar3 = () => {
+        // document.getElementById("s1").style.backgroundColor = "red !important";
+        setStar1("red");
+        setStar2("red");
+        setStar3("red");
+    }
+
+    const OnBackgroundStar4 = () => {
+        // document.getElementById("s1").style.backgroundColor = "red !important";
+        setStar1("red");
+        setStar2("red");
+        setStar3("red");
+        setStar4("red");
+    }
+
+    const OnBackgroundStar5 = () => {
+        // document.getElementById("s1").style.backgroundColor = "red !important";
+        setStar1("red");
+        setStar2("red");
+        setStar3("red");
+        setStar4("red");
+        setStar5("red");
+    }
+
+
+    const OffBackgroundStar = () => {
+        if (star1 === "red") {
+            setStar1("gray")
+            setStar2("gray")
+            setStar3("gray")
+            setStar4("gray")
+            setStar5("gray")
+        }
+    }
     const fillForm = (e) => {
         setSearchData({ [e.target.name]: e.target.value })
     }
@@ -63,15 +115,29 @@ const Search = () => {
                         <div className='memes_box'>
                             {memes.postsResults.map(elmnt => {
                                 return (
-                                    <>
-                                        <div className="meme_card" key={elmnt._id}>
-                                            <img className='meme_photo' src={elmnt.img} alt={elmnt.title} />
-                                            <div className="meme_name">{elmnt.title}</div>
-                                            <div className="meme_rating_action">rate me!</div>
-                                            {/* <div className="meme_rating">rating: {elmnt.rating}</div> */}
-                                            <div className="meme_creator">meme done by: {elmnt.ownerNickname}</div>
+
+                                    <div className='meme_card' key={elmnt._id}>
+                                        <img className='meme_photo' src={elmnt.img} alt={elmnt.title} />
+                                        <div className="meme_name">{elmnt.title}</div>
+                                        <div className="meme_rating_action" onMouseOver={() => OffBackgroundStar()}>
+                                            <div className="meme_rating_star" style={{ backgroundColor: star1 }} onMouseOver={() => OnBackgroundStar()}>
+                                                <div className="star_white"></div>
+                                            </div>
+                                            <div className="meme_rating_star" style={{ backgroundColor: star2 }} onMouseOver={() => OnBackgroundStar2()}>
+                                                <div className="star_white"></div>
+                                            </div>
+                                            <div className="meme_rating_star" style={{ backgroundColor: star3 }} onMouseOver={() => OnBackgroundStar3()}>
+                                                <div className="star_white"></div></div>
+                                            <div className="meme_rating_star" style={{ backgroundColor: star4 }} onMouseOver={() => OnBackgroundStar4()}>
+                                                <div className="star_white"></div>
+                                            </div>
+                                            <div className="meme_rating_star" style={{ backgroundColor: star5 }} onMouseOver={() => OnBackgroundStar5()}>
+                                                <div className="star_white"></div>
+                                            </div>
                                         </div>
-                                    </>
+                                        <div className="meme_rating">rating: {elmnt.ratingAverage}</div>
+                                        <div className="meme_creator">meme done by: {elmnt.ownerNickname}</div>
+                                    </div>
                                 )
                             })}
                         </div>
@@ -86,40 +152,80 @@ const Search = () => {
         }
     }
 
+    if (memes.length == 0) {
+        return (
+            <div className="container_box" id="search_box">
 
-    return (
-        <div className="container_box" id="search_box">
 
+                {/* FIXED SEARCH TRANSPARENCY  */}
+                <div className="container_search_transparency">
+                    search here
+                </div>
 
-            {/* FIXED SEARCH TRANSPARENCY  */}
-            <div className="container_search_transparency">
-                search here
-            </div>
+                {/* THIS WILL BE A CAROUSEL OF IMAGES */}
 
-            {/* THIS WILL BE A CAROUSEL OF IMAGES */}
+                <div className="component_search" id="animationContainerFromTop">
 
-            <div className="component_search" id="animationContainerFromTop">
+                    <div className="login_inputs">
 
-                <div className="login_inputs">
+                        <input
+                            type="text"
+                            placeholder='Introduce term..'
+                            name="term"
+                            title="nick"
+                            autoComplete="off"
+                            onChange={(e) => { fillForm(e) }}
+                        />
+                        <div
+                            className="searchBttn"
+                            onClick={() => search()}
 
-                    <input
-                        type="text"
-                        placeholder='Introduce term..'
-                        name="term"
-                        title="nick"
-                        autoComplete="off"
-                        onChange={(e) => { fillForm(e) }}
-                    />
-                    <div
-                        className="searchBttn"
-                        onClick={() => search()}
-
-                    >GO!
+                        >GO!
+                        </div>
                     </div>
                 </div>
-                {memesRender()}
             </div>
-        </div>
-    )
+        )
+    } else {
+        if (memes.postsResults.length !== 0) {
+            console.log(memes.postsResults);
+            return (
+                <div className="component_search" id="animationContainerFromTop">
+                    <div className='component_box memes_component'>
+                        <div className='memes_box'>
+                            {memes.postsResults.map(elmnt => {
+                                return (
+                                    <div className='meme_card' key={elmnt._id}>
+                                        <img className='meme_photo' src={elmnt.img} alt={elmnt.title} />
+                                        <div className="meme_name">{elmnt.title}</div>
+                                        <div className="meme_rating_action" onMouseOver={() => OffBackgroundStar()}>
+                                            <div className="meme_rating_star" style={{ backgroundColor: star1 }} onMouseOver={() => OnBackgroundStar()}>
+                                                <div className="star_white"></div>
+                                            </div>
+                                            <div className="meme_rating_star" style={{ backgroundColor: star2 }} onMouseOver={() => OnBackgroundStar2()}>
+                                                <div className="star_white"></div>
+                                            </div>
+                                            <div className="meme_rating_star" style={{ backgroundColor: star3 }} onMouseOver={() => OnBackgroundStar3()}>
+                                                <div className="star_white"></div></div>
+                                            <div className="meme_rating_star" style={{ backgroundColor: star4 }} onMouseOver={() => OnBackgroundStar4()}>
+                                                <div className="star_white"></div>
+                                            </div>
+                                            <div className="meme_rating_star" style={{ backgroundColor: star5 }} onMouseOver={() => OnBackgroundStar5()}>
+                                                <div className="star_white"></div>
+                                            </div>
+                                        </div>
+                                        <div className="meme_rating">rating: {elmnt.ratingAverage}</div>
+                                        <div className="meme_creator">meme done by: {elmnt.ownerNickname}</div>
+                                    </div>
+                                )
+                            })}
+                        </div>
+                    </div>
+                </div>
+
+
+            )
+        }
+    }
 }
 export default Search;
