403196022fd4e717333261b5a4fa0bcedd354edb
jmonloop <monleonlopezjavier@gmail.com>
2022-03-27 12:54:42 +0200

    search styles improved

--- src/Containers/Search/Search.jsx
+++ src/Containers/Search/Search.jsx
@@ -111,92 +111,12 @@ const Search = () => {
 
     const memesRender = () => {
         if (memes.length !== 0) {
-            if (memes.postsResults.length !== 0) {
-                return (
-                    <div className='component_box memes_component'>
-                        <div className='memes_box'>
-                            {memes.postsResults.map(elmnt => {
-                                return (
-
-                                    <div className='meme_card' key={elmnt._id}>
-                                        <img className='meme_photo' src={elmnt.img} alt={elmnt.title} />
-                                        <div className="meme_name">{elmnt.title}</div>
-                                        <div className="meme_rating_action" onMouseOver={() => OffBackgroundStar()}>
-                                            <div className="meme_rating_star" style={{ backgroundColor: star1 }} onMouseOver={() => OnBackgroundStar()}>
-                                                <div className="star_white"></div>
-                                            </div>
-                                            <div className="meme_rating_star" style={{ backgroundColor: star2 }} onMouseOver={() => OnBackgroundStar2()}>
-                                                <div className="star_white"></div>
-                                            </div>
-                                            <div className="meme_rating_star" style={{ backgroundColor: star3 }} onMouseOver={() => OnBackgroundStar3()}>
-                                                <div className="star_white"></div></div>
-                                            <div className="meme_rating_star" style={{ backgroundColor: star4 }} onMouseOver={() => OnBackgroundStar4()}>
-                                                <div className="star_white"></div>
-                                            </div>
-                                            <div className="meme_rating_star" style={{ backgroundColor: star5 }} onMouseOver={() => OnBackgroundStar5()}>
-                                                <div className="star_white"></div>
-                                            </div>
-                                        </div>
-                                        <div className="meme_rating">rating: {elmnt.ratingAverage}</div>
-                                        <div className="meme_creator">meme done by: {elmnt.ownerNickname}</div>
-                                    </div>
-                                )
-                            })}
-                        </div>
-                    </div>
-                )
-            }
-
-        } else {
             return (
-                <></>
-            )
-        }
-    }
-
-    if (memes.length == 0) {
-        return (
-            <div className="container_box" id="search_box">
-
-
-                {/* FIXED SEARCH TRANSPARENCY  */}
-                <div className="container_search_transparency">
-                    search here
-                </div>
-
-                {/* THIS WILL BE A CAROUSEL OF IMAGES */}
-
-                <div className="component_search" id="animationContainerFromTop">
-
-                    <div className="login_inputs">
-
-                        <input
-                            type="text"
-                            placeholder='Introduce term..'
-                            name="term"
-                            title="nick"
-                            autoComplete="off"
-                            onChange={(e) => { fillForm(e) }}
-                        />
-                        <div
-                            className="searchBttn"
-                            onClick={() => search()}
-
-                        >GO!
-                        </div>
-                    </div>
-                </div>
-            </div>
-        )
-    } else {
-
-        return (
-            <div className="component_search" id="animationContainerFromTop">
                 <ScrollArea className='scrollArea'>
-                    <Accordion iconPosition="right" iconSize={0} offsetIcon={false}>
+                    <Accordion className='accordion' iconPosition="right" iconSize={0} offsetIcon={false}>
                         <Accordion.Item label={`Users results ... +`}>
-                            <div className='accordion'>
-                                {memes.usersResults.map(elmnt => {
+                            <div className='accordionContent'>
+                                {memes.usersResults?.map(elmnt => {
                                     return (
                                         <div className="userResult">{elmnt.nickname}</div>
                                     )
@@ -205,10 +125,10 @@ const Search = () => {
                         </Accordion.Item>
                     </Accordion>
 
-                    <Accordion iconPosition="right" iconSize={0} offsetIcon={false}>
+                    <Accordion className='accordion' iconPosition="right" iconSize={0} offsetIcon={false}>
                         <Accordion.Item label={`Posts results ... +`}>
-                            <div className='accordion'>
-                                {memes.postsResults.map(elmnt => {
+                            <div className='accordionContent'>
+                                {memes.postsResults?.map(elmnt => {
                                     return (
 
                                         <div className='meme_card' key={elmnt._id}>
@@ -244,11 +164,50 @@ const Search = () => {
 
 
                 </ScrollArea>
+            )
+
+        } else {
+            return (
+                <></>
+            )
+        }
+    }
+
+
+    return (
+        <div className="container_box" id="search_box">
+
+
+            {/* FIXED SEARCH TRANSPARENCY  */}
+            <div className="container_search_transparency">
+                search here
             </div>
 
+            {/* THIS WILL BE A CAROUSEL OF IMAGES */}
 
-        )
+            <div className="component_search" id="animationContainerFromTop">
 
-    }
+                <div className="login_inputs">
+
+                    <input
+                        type="text"
+                        placeholder='Introduce term..'
+                        name="term"
+                        title="nick"
+                        autoComplete="off"
+                        onChange={(e) => { fillForm(e) }}
+                    />
+                    <div
+                        className="searchBttn"
+                        onClick={() => search()}
+
+                    >GO!
+                    </div>
+                </div>
+                {memesRender()}
+            </div>
+        </div>
+    )
+
 }
 export default Search;
