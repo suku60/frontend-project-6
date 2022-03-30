603c741c7b4e87cfd421907e5218d25efa3d510e
jmonloop <monleonlopezjavier@gmail.com>
2022-03-31 00:29:17 +0200

    get latest 10 memes added to home. Working on load more

--- src/Containers/Home/Home.jsx
+++ src/Containers/Home/Home.jsx
@@ -1,8 +1,9 @@
-import React, {useEffect, useRef, useState} from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import axios from 'axios';
 import { Navigate, useNavigate } from 'react-router-dom';
 
-import {ReactComponent as StarSvg} from '../../assets/svg/star.svg'
+import { ReactComponent as StarSvg } from '../../assets/svg/star.svg'
+import { Accordion } from '@mantine/core';
 
 import './Home.css';
 
@@ -12,6 +13,7 @@ const Home = () => {
     // HOOKS
     // memes
     const [memes, setMemes] = useState([]);
+    const [page, setPage] = useState(0);
 
     // stars
     const [star1, setStar1] = useState("var(--color-vapor-3-3");
@@ -24,10 +26,10 @@ const Home = () => {
     const topline = useRef(null);
 
     // USEEFFECTS
-    useEffect(()=> {
-        bringMemes();
+    useEffect(() => {
+        bringMemes(0);
 
-    },[])
+    }, [])
 
     // FUNCTIONS
     // Handlers
@@ -37,29 +39,29 @@ const Home = () => {
         console.log("scrolling back?")
     }
     // stars handlers
-    const OnBackgroundStar = () =>{
+    const OnBackgroundStar = () => {
         setStar1("var(--color-vapor-1-3)")
     }
 
-    const OnBackgroundStar2 = () =>{
+    const OnBackgroundStar2 = () => {
         setStar1("var(--color-vapor-1-3)");
         setStar2("var(--color-vapor-1-3)")
     }
-    
-    const OnBackgroundStar3 = () =>{
+
+    const OnBackgroundStar3 = () => {
         setStar1("var(--color-vapor-1-3)");
         setStar2("var(--color-vapor-1-3)");
         setStar3("var(--color-vapor-1-3)");
     }
 
-    const OnBackgroundStar4 = () =>{
+    const OnBackgroundStar4 = () => {
         setStar1("var(--color-vapor-1-3)");
         setStar2("var(--color-vapor-1-3)");
         setStar3("var(--color-vapor-1-3)");
         setStar4("var(--color-vapor-1-3)");
     }
 
-    const OnBackgroundStar5 = () =>{
+    const OnBackgroundStar5 = () => {
         setStar1("var(--color-vapor-1-3)");
         setStar2("var(--color-vapor-1-3)");
         setStar3("var(--color-vapor-1-3)");
@@ -68,8 +70,8 @@ const Home = () => {
     }
 
 
-    const OffBackgroundStar = () =>{
-        if(star1 === "var(--color-vapor-1-3)"){
+    const OffBackgroundStar = () => {
+        if (star1 === "var(--color-vapor-1-3)") {
             setStar1("var(--color-vapor-3-3)")
             setStar2("var(--color-vapor-3-3)")
             setStar3("var(--color-vapor-3-3)")
@@ -85,30 +87,24 @@ const Home = () => {
 
     // Local functions
     // Axios call to map images
-    const bringMemes = async () => {
+    const bringMemes = async (page) => {
 
         try {
 
-            let memeResponse = await axios.get('https://api.imgflip.com/get_memes');
+            let results = await axios.get(`https://socialmeme.herokuapp.com/posts/get10?page=${page}`);
 
-            // console.log("memeresponsee here", memeResponse)
-            
-            // console.log("memeresponsee 2 here", memeResponse.data.data)
-
-
-            let lessMemes = memeResponse.data.data.memes.slice(0, 10)
 
             // console.log("less?", lessMemes)
 
-
-            setMemes(lessMemes)
+            console.log("soy yo", results.data)
+            setMemes(results.data)
 
             // console.log("memes¿", memes)
 
             // setMemes(memeResponse.data.results)
 
 
-        }catch(error){
+        } catch (error) {
             console.log(error)
         }
 
@@ -116,63 +112,102 @@ const Home = () => {
 
     // console.log("mememess?", memes)
 
-return (
-    <div className="container_box" id="home_box">
-        
+    return (
+        <div className="container_box" id="home_box">
 
-{/* FIXED HOME TRANSPARENCY  */}
-{/* THIS WILL BE A CAROUSEL OF IMAGES */}
 
-        <div className="component_home" id="animationContainerFromTop">
-            <div className='component_box memes_component'>
-                <div className='memes_box' style={{transition : ".3s"}}>
-                    <div ref={topline} className="home_topline">
-                    </div>
-                    <div className="container_home_transparency" onClick={scrollToTop}>
-                        <div className="home_transparency_text">
-                            t o p
+            {/* FIXED HOME TRANSPARENCY  */}
+            {/* THIS WILL BE A CAROUSEL OF IMAGES */}
+
+            <div className="component_home" id="animationContainerFromTop">
+                <div className='component_box memes_component'>
+                    <div className='memes_box' style={{ transition: ".3s" }}>
+                        <div ref={topline} className="home_topline">
                         </div>
-                    </div>
-                    {memes?.map(images => {
-                        return (
-                             
-                        <div className='meme_card' key={images.id}>
-                            <img className='meme_photo' src={images.url} alt={images.name}/>
-                            <div className="meme_card_data">
-                                    
-                                <div className="meme_name">{images.name}
-                                </div>
-                                <div className="meme_rating_action" onMouseOver={()=>OffBackgroundStar()}>
-                                    <div className="meme_rating_star" style={{backgroundColor : star1}} onMouseOver={()=>OnBackgroundStar()}>
-                                        <StarSvg  style={{backgroundColor : star1}} onMouseOver={()=>OnBackgroundStar()}/>
-                                        {/* <div className="star_white"></div> */}
-                                    </div>
-                                    <div className="meme_rating_star" style={{backgroundColor : star2}} onMouseOver={()=>OnBackgroundStar2()}>
-                                            <StarSvg  style={{backgroundColor : star2}} onMouseOver={()=>OnBackgroundStar2()}/>
-                                    </div>
-                                    <div className="meme_rating_star" style={{backgroundColor : star3}} onMouseOver={()=>OnBackgroundStar3()}>
-                                            <StarSvg  style={{backgroundColor : star3}} onMouseOver={()=>OnBackgroundStar3()}/>
-                                    </div>                             
-                                    <div className="meme_rating_star" style={{backgroundColor : star4}} onMouseOver={()=>OnBackgroundStar4()}>
-                                        <StarSvg  style={{backgroundColor : star4}} onMouseOver={()=>OnBackgroundStar4()}/>
-                                    </div>
-                                    <div className="meme_rating_star" style={{backgroundColor : star5}} onMouseOver={()=>OnBackgroundStar5()}>
-                                            <StarSvg  style={{backgroundColor : star5}} onMouseOver={()=>OnBackgroundStar5()}/>
-                                    </div>
-                                </div>
-                                <div className="meme_rating">rating: {images.box_count}
-                                </div>
-                            <div className="meme_creator">meme done by: {images.id}
+                        <div className="container_home_transparency" onClick={scrollToTop}>
+                            <div className="home_transparency_text">
+                                t o p
                             </div>
-                            <div className="meme_card_footer"></div>
-                            </div> 
                         </div>
-                    )
-                    })}
+                        {memes?.map(images => {
+                            return (
+
+                                <div className='meme_card' key={images._id}>
+                                    <img className='meme_photo' src={images.img} alt={images.title} />
+                                    <div className="meme_card_data">
+
+                                        <div className="meme_name">{images.title}
+                                        </div>
+                                        <div className="meme_name">{images.text}
+                                        </div>
+                                        <div className="meme_rating_action" onMouseOver={() => OffBackgroundStar()}>
+                                            <div className="meme_rating_star" style={{ backgroundColor: star1 }} onMouseOver={() => OnBackgroundStar()}>
+                                                <StarSvg style={{ backgroundColor: star1 }} onMouseOver={() => OnBackgroundStar()} />
+                                                {/* <div className="star_white"></div> */}
+                                            </div>
+                                            <div className="meme_rating_star" style={{ backgroundColor: star2 }} onMouseOver={() => OnBackgroundStar2()}>
+                                                <StarSvg style={{ backgroundColor: star2 }} onMouseOver={() => OnBackgroundStar2()} />
+                                            </div>
+                                            <div className="meme_rating_star" style={{ backgroundColor: star3 }} onMouseOver={() => OnBackgroundStar3()}>
+                                                <StarSvg style={{ backgroundColor: star3 }} onMouseOver={() => OnBackgroundStar3()} />
+                                            </div>
+                                            <div className="meme_rating_star" style={{ backgroundColor: star4 }} onMouseOver={() => OnBackgroundStar4()}>
+                                                <StarSvg style={{ backgroundColor: star4 }} onMouseOver={() => OnBackgroundStar4()} />
+                                            </div>
+                                            <div className="meme_rating_star" style={{ backgroundColor: star5 }} onMouseOver={() => OnBackgroundStar5()}>
+                                                <StarSvg style={{ backgroundColor: star5 }} onMouseOver={() => OnBackgroundStar5()} />
+                                            </div>
+                                        </div>
+                                        <div className="meme_rating">rating: {images.ratingAverage}
+                                        </div>
+                                        <div className="meme_creator">meme done by: {images.ownerNickname}
+                                        </div>
+                                        <Accordion className='meme_comments_accordion' iconPosition="right" iconSize={0} offsetIcon={false}>
+                                            <Accordion.Item label={`Comments`}>
+                                                <div className='accordionContent'>
+                                                    {images.comments.map(elmnt => {
+                                                        return (
+                                                            <div className='meme_comment_box'>
+                                                                <div className="meme_comment_owner">{elmnt.ownerNickname}</div>
+                                                                <div className="meme_comment_created">{elmnt.created}</div>
+                                                                <div className="meme_comment_content">{elmnt.comment}</div>
+                                                                <div className="meme_comment_rating_action" onMouseOver={() => OffBackgroundStar()}>
+                                                                    <div className="meme_comment_rating_star" style={{ backgroundColor: star1 }} onMouseOver={() => OnBackgroundStar()}>
+                                                                        <StarSvg style={{ backgroundColor: star1 }} onMouseOver={() => OnBackgroundStar()} />
+                                                                        {/* <div className="star_white"></div> */}
+                                                                    </div>
+                                                                    <div className="meme_comment_rating_star" style={{ backgroundColor: star2 }} onMouseOver={() => OnBackgroundStar2()}>
+                                                                        <StarSvg style={{ backgroundColor: star2 }} onMouseOver={() => OnBackgroundStar2()} />
+                                                                    </div>
+                                                                    <div className="meme_comment_rating_star" style={{ backgroundColor: star3 }} onMouseOver={() => OnBackgroundStar3()}>
+                                                                        <StarSvg style={{ backgroundColor: star3 }} onMouseOver={() => OnBackgroundStar3()} />
+                                                                    </div>
+                                                                    <div className="meme_comment_rating_star" style={{ backgroundColor: star4 }} onMouseOver={() => OnBackgroundStar4()}>
+                                                                        <StarSvg style={{ backgroundColor: star4 }} onMouseOver={() => OnBackgroundStar4()} />
+                                                                    </div>
+                                                                    <div className="meme_comment_rating_star" style={{ backgroundColor: star5 }} onMouseOver={() => OnBackgroundStar5()}>
+                                                                        <StarSvg style={{ backgroundColor: star5 }} onMouseOver={() => OnBackgroundStar5()} />
+                                                                    </div>
+                                                                </div>
+                                                                <div className="meme_comments_rating">rating: {elmnt.ratingAverage}
+                                                                </div>
+                                                            </div>
+
+                                                        )
+                                                    })}
+                                                </div>
+                                            </Accordion.Item>
+                                        </Accordion>
+                                        <div className="meme_card_footer"></div>
+                                    </div>
+                                </div>
+                            )
+                        })}
+                        <div className='memes_loadMore_bttn'>Load More</div>
+                    </div>
                 </div>
             </div>
-        </div>           
-    </div>
+        </div>
     )
 }
 export default Home;
